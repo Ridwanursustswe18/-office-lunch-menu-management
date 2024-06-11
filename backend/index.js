f8f267f9b10d78721e9fa46ef60ed7915a74a42c
Ridwan <ridwanur196@gmail.com>
2024-06-11 22:27:06 +0600

    user creation api added

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors'); 
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.BACKEND_PORT;
 
+app.use(bodyParser.json());
 app.use(cors()); 
-
+app.use(express.json());
+const userRoutes = require('./routes/userRoutes');
+app.use('/api/users', userRoutes);
 app.get('/', async (req, res) => {
   try {
     res.send('Database Connected Successfully');
