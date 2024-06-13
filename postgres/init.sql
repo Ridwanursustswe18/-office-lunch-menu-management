dced85597d0f2d4087082b7490ead987140c788e
Ridwan <ridwanur196@gmail.com>
2024-06-13 22:01:22 +0600

    fixed db schema

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,4 +1,3 @@
--- Create the "daily_menu" table
 CREATE TABLE menu (
     id SERIAL PRIMARY KEY,
     menu_date DATE,
@@ -6,13 +5,16 @@ CREATE TABLE menu (
     description TEXT
     
 );
--- Create the "users" table
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     full_name VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     user_role BOOLEAN NOT NULL DEFAULT TRUE,
-    menu_id INTEGER REFERENCES menu(id),
     designation VARCHAR(255),
     password VARCHAR(255) NOT NULL
+);
+CREATE TABLE user_menu (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    menu_id INTEGER REFERENCES menu(id)
 );
